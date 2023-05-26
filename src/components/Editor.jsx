5d6f47fb639557e634eca877ca45e23bec7a9e9d
Craig Olson <craig.olson804@gmail.com>
2023-05-26 16:25:23 -0700

    upload post to firestore

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
-import ReactQuill from "react-quill";
+import { useState, useEffect } from "react";
+import ReactQuill, { Quill } from "react-quill";
+import { collection, addDoc } from "firebase/firestore";
+import { firestore } from "../firebase/config.js";
 import "react-quill/dist/quill.snow.css";
 
 const Editor = () => {
   const [value, setValue] = useState("");
 
-  const onSubmit = () => {
+  const onSubmit = async () => {
     console.log(value);
+    try {
+      const createdAt = new Date();
+
+      const postRef = await addDoc(collection(firestore, "posts"), {
+        title: "Title!",
+        body: value,
+        created_at: createdAt,
+      });
+      console.log("post uploaded successfully with ID: ", postRef.id);
+    } catch (error) {
+      console.error("Error uploading post:", error);
+    }
   };
 
   return (
